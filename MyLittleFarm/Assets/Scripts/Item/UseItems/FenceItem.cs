e095768ff5c7d5e0ceb0c516390df03e788d56db
nuununuun <triplecode@kookmin.ac.kr>
2020-03-25 17:40:59 +0900

    설치형 아이템 설치 범위 내 다른 오브젝트 있을 때 설치할 수 없게 하는 기능 구현

--- MyLittleFarm/Assets/Scripts/Item/UseItems/FenceItem.cs
+++ MyLittleFarm/Assets/Scripts/Item/UseItems/FenceItem.cs
@@ -7,10 +7,20 @@ public class FenceItem : IN.GridBaseItem {
 
     public FenceObject fencePrefab;
 
+    public LayerMask tempMask;
+
     public float range = 1.5f;
+
+    private BoxCollider2D sizeCollider;
 
     public override void Activated(CharacterController2D controller) {
         GameManager.Instance.itemPreview.gameObject.SetActive(true);
+
+        // 나중에 아이템 데이터베이스 매니저 같은데서 아이템 내 사이즈 콜라이더 캐싱 해야 함
+        foreach (var col in fencePrefab.GetComponentsInChildren<BoxCollider2D>()) {
+            Debug.Log(col.tag);
+            if (col.CompareTag("Size")) sizeCollider = col;
+        }
     }
 
     public override void Deactivated(CharacterController2D controller) {
@@ -21,13 +31,22 @@ public class FenceItem : IN.GridBaseItem {
         var gridPosition = Vector2Int.FloorToInt(mousePosition) + Vector2.one * 0.5f;
         var characterPosition = Vector2Int.FloorToInt(controller.transform.position) + Vector2.one * 0.5f;
 
-        var len = (characterPosition - gridPosition).magnitude;
-
         GameManager.Instance.itemPreview.sprite.sprite = fenceSprites[3];
-
         GameManager.Instance.itemPreview.transform.position = new Vector3(gridPosition.x, gridPosition.y, controller.transform.position.z);
 
-        if (len <= range) {
+        var len = (characterPosition - gridPosition).magnitude;
+
+        // 모든 조건 충족 한 경우 아이템 놓기 가능
+        bool condCheck = true;
+
+        // 거리 체크
+        if (len > range) condCheck = false;
+
+        // 겹치는 오브젝트 있는지 체크
+        var hit = Physics2D.OverlapBox(gridPosition + sizeCollider.offset, sizeCollider.size - Vector2.one * 0.1f, 0, tempMask);
+        if (hit) condCheck = false;
+
+        if (condCheck) { 
             GameManager.Instance.itemPreviewIndicator.color = Color.green;
         } else {
             GameManager.Instance.itemPreviewIndicator.color = Color.red;
