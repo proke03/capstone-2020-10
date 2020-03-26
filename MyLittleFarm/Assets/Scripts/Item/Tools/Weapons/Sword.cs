274e6598149cecb84975c30cf684ddccaae2d4ab
nuununuun <triplecode@kookmin.ac.kr>
2020-03-26 20:10:53 +0900

    계획서 발표 pdf 자료 업로드

---
+++ MyLittleFarm/Assets/Scripts/Item/Tools/Weapons/Sword.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IN {
+
+    public class Sword : NonGridBaseItem {
+        public override IEnumerator Use(CharacterController2D controller, Vector2 mousePosition) {
+            yield return null;
+        }
+    }
+
+}
