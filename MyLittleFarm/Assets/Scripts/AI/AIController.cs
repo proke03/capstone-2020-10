db9652b2683792beaa00972df95526385af7961f
proke03 <proke03@naver.com>
2020-04-08 11:42:51 +0900

    AI 작업 중 저장소 이동하기 위한 commit

---
+++ MyLittleFarm/Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    enum AIState
+    {
+        Wait,   //대기
+        Roam,   //배회
+        Chase,  //공격하려고 다가감
+        Attack
+    }
+
+    AIState state = AIState.Wait;
+
+    private void Awake()
+    {
+        //상태 초기화
+    }
+    private void Start()
+    {
+        StartCoroutine("FSM");  //FSM 코루틴 실행.
+    }
+
+    private IEnumerator FSM() {
+        while (true) {
+            yield return StartCoroutine(state.ToString());
+        }
+    }
+}
